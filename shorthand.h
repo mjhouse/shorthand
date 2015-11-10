dc3be1539e92e3e704e81b35151549813cc82c67
Michael House <blankstamped@gmail.com>
2015-11-10 15:37:24 -0600

    completed recursive copy function

--- shorthand.h
+++ shorthand.h
@@ -288,21 +288,26 @@ namespace shorthand {
 		}
 
 /*
+ *	Copy:
+ * 			recursively copies a directory structure to another location, aborts recursion if backup
+ * 			location is a child of the input location.
  * 
- * 
- * 
- * 
+ * 	USAGE:
+ * 			first arg is char* to input path (directory to be backed up), second argument is char* to
+ * 			output directory (location to copy structur too). 
  */
- 	
-		int copy(char *in/*, char *out*/){
-			if(exists(in)!=-1&&exists(in)==1/*&&exists(out)==1*/){
+		
+		int copy(char *in, char *out){
+			if(exists(in)!=-1&&exists(in)==1&&exists(out)==1){ // both paths have to be dir
 				struct dirent *entry;
 				DIR *dirp = opendir(in);
 				while(entry=readdir(dirp)){
 					
 					// Copy current path
-					char *fp = (char *)malloc(sizeof(in)+sizeof(entry->d_name)+2);
+					char *fp = (char *)malloc(sizeof(in)+sizeof(entry->d_name)+1);
+					char *fo = (char *)malloc(sizeof(out)+sizeof(entry->d_name)+1);
 					strcpy(fp,in);
+					strcpy(fo,out);
 					
 					// Check for trailing "/" and add it if it's not there
 					int j=0;
@@ -311,19 +316,36 @@ namespace shorthand {
 					if(fp[j]!='/')
 						strcat(fp,"/");
 					
+					j=0;
+					for(int i=0;out[i]!='\0';i++)
+						if(out[i+1]=='\0') j=i;
+					if(fo[j]!='/')
+						strcat(fo,"/");
+					
+					
 					// Add the file name if it isn't ".." or "."
 					if(strcmp(entry->d_name, "..")!=0&&strcmp(entry->d_name, ".")!=0){
 						strcat(fp, entry->d_name);
+						strcat(fo, entry->d_name);
 						if(exists(fp)==1){
-							// IS DIR
-							printf("DIR: %s\n", fp);
+							// IS DIR: duplicate directory structure at out
+							mkdir(fo, 0700);
+							if(strncmp(fp,fo,j)!=0){
+								printf("DIR: %s\n", fp);
+								copy(fp,fo);	
+							}
 						}
 						else if(exists(fp)==0){
-							// IS FILE
-							printf("FILE: %s\n", fp);
+							// IS FILE: copy file
+							//printf("FILE: %s\n", fp);
+							std::ifstream i(fp, std::ios::binary);
+							std::ofstream o(fo, std::ios::binary);
+							o << i.rdbuf();
+							o.close();i.close();
 						}
 					}
 					// free memory allocated to fp
+					free(fo);
 					free(fp);
 				}
 				return 1;
@@ -335,9 +357,9 @@ namespace shorthand {
 
 /*
  *	TODO:
- * 			make a copy function that recursively copies from a given directory down
- * 			make a simplified exec function 
- * 			
+ * 			1. make a simplified exec function 
+ * 			2. make compression function
+ * 			3. possibly push copy, exists, compression functions out into different header
  */
 
 }
